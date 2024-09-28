58d87648981aa717887c44fa7acbfa08edbca8a6
Jerry Wang <jerryw28@nycstudents.net>
2024-09-27 22:30:46 -0400

    Got my returnCopy to work with two large cases; I checked their adresses and they were different as well. Should be correct. Also wrote without test cases my concatArry code.

--- 2024-09-27/arrayToString.java
+++ 2024-09-27/arrayToString.java
@@ -1,12 +1,26 @@
 public class arrayToString{
   public static void main (String args[]){
-    // arrayToString test cases
-    System.out.println(arrayToString(new int[]{1, 2, 3, 1227})); //Worked
-    System.out.println(arrayToString(new int[]{1, 2, 3, 1227, -4})); //Worked
-    System.out.println(arrayToString(new int[]{})); //Worked
-    //  sameArray method test cases
-    System.out.println(sameArray(new int[]{1, 2, 3, 1227, -4}, new int[]{1, 22, 3, 1227, -4})); //Did not work
+    // turnArrayToString test cases
+    System.out.println(turnArrayToString(new int[]{1, 2, 3, 1227})); //Worked
+    System.out.println(turnArrayToString(new int[]{1, 2, 3, 1227, -4})); //Worked
+    System.out.println(turnArrayToString(new int[]{})); //Worked
+    // sameArray method test cases
+    System.out.println(sameArray(new int[]{1, 2, 3, 1227, 1227, -4}, new int[]{1, 2, 3, 1227, -4})); //Worked
     System.out.println(sameArray(new int[]{1, 2, 3, 1227, -4}, new int[]{1, 2, 3, 1227, -4})); //Worked
+    // returnCopy method test cases
+    int[] copyTest = new int[]{3,5,6,7,8,9,10,1227,520,180};
+    System.out.println(sameArray(copyTest, returnCopy(copyTest))); //Worked
+    System.out.println(copyTest);
+    System.out.println(returnCopy(copyTest));
+    System.out.println(turnArrayToString(copyTest));
+    System.out.println(turnArrayToString(returnCopy(copyTest)));
+    copyTest = new int[] {-5, 67, 1230, -1234, 1227, 1227, -5, 7, 8 , 9 , 0};
+    System.out.println(sameArray(copyTest, returnCopy(copyTest))); //Worked
+    System.out.println(copyTest);
+    System.out.println(returnCopy(copyTest));
+    System.out.println(turnArrayToString(copyTest));
+    System.out.println(turnArrayToString(returnCopy(copyTest)));
+    // 
 
   }
 
@@ -28,7 +42,7 @@ public class arrayToString{
     }
   }
 
-  public static String arrayToString (int[] nums){
+  public static String turnArrayToString (int[] nums){
     String s = "[";
     for (int i = 0; i < nums.length; i++){
       s += nums[i];
@@ -38,12 +52,23 @@ public class arrayToString{
     }
     return s + "]";
   }
+  public static int[] returnCopy(int[]ary) {
+    int[] array = new int[ary.length];
+    for (int i = 0; i < ary.length; i++){
+      array[i] = ary[i];
+    }
+    return array;
+  }
+  
+  public static int[] concatArray(int[]ary1,int[]ary2){
+    int[] array = new int[ary1.length + ary2.length];
+    for (int i = 0; i < ary1.length; i++){
+      array[i] = ary1[i];
+    }
+    for (int i = 0; i < ary2.length; i++){
+      array[i+ary1.length] = ary2[i];
+    }
+    return array;
+  }
 }
 
-public static int[] returnCopy(int[]ary) {
-
-}
-
-public static int[] concatArray(int[]ary1,int[]ary2){
-
-}
