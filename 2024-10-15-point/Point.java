8cf8390cbf821367b08953345876b45278a4c10e
Jerry Wang <jerryw28@nycstudents.net>
2024-10-15 11:29:35 -0400

    Finished and tested second Point constructor.

--- 2024-10-15-point/Point.java
+++ 2024-10-15-point/Point.java
@@ -8,6 +8,8 @@ public class Point{
   //Initialize this Point to have the same values as the other Point.
   public Point(Point other){
     //COMPLETE THIS SECOND
+    this.x = other.getX();
+    this.y = other.getY();
     //Then write test cases.
   }
 
