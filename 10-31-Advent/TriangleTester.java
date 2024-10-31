35385c44e2f8299bf139a4112b7141f582b85615
Jerry Wang <jerryw28@nycstudents.net>
2024-10-31 11:58:08 -0400

    Wrote code that doesn't work. Going to fix.

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,8 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+
 public class TriangleTester{
-  Scanner sc = new Scanner("Some String (or File) Here");
+   public static void main(String[] args){
+     System.out.println(countTrianglesA("inputTri.txt");
+   }
 
 
 
+   public static int countTrianglesA(String filename){
+     int a = 0;
+     int b = 0;
+     int c =0;
+     try {
+       File file = new File(filename);
+       Scanner input = new Scanner(file);
+     input.close();//releases the file from your program
+
+     } catch (FileNotFoundException ex) {
+     //File not found what should you do?
+     System.out.println("File not found");
+     return; //you can return from a void function just don't put a value.
+     }
+     int temp = 0;
+
+     Scanner input = new Scanner(filename);
+     while(input.hasNext()){
+       if (triangleExist()
+
+     }
+
+   }
+   public static boolean triangleExist(String lengths){
+
+   }
 }
