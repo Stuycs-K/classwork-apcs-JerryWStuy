b268b0ca254ee329fc6d85267fb019f2a2b8f26c
Jerry Wang <jerryw28@nycstudents.net>
2024-10-31 21:16:39 -0400

    Wrote some text files and made some changes.

--- 10-31-Advent/Main.java
+++ 10-31-Advent/Main.java
@@ -6,20 +6,19 @@ class Main {
 
       // creates a Scanner object to read throug the String
       Scanner input = new Scanner(text);
-
       //you don't want a for loop here unless you know the size of the data set
-
       while(input.hasNext()){
         System.out.println(input.next());
       }
       System.out.println(text);
+      input.close();
 
       Scanner sc1 = new Scanner("This is a bunch of words");
-
       while(sc1.hasNext()){
         System.out.println(sc1.next());
       }
       System.out.println(sc1);
+      sc1.close();
 
       Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
       int temp = 0;
@@ -27,5 +26,6 @@ class Main {
         temp += sc2.nextDouble();
       }
       System.out.println(temp);
+      sc2.close();
     }
   }
