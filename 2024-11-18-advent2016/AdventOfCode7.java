035ef27dfe401b0d50e57b9284c927d0d120cf9c
Jerry Wang <jerryw28@nycstudents.net>
2024-11-19 19:19:38 -0500

    Took me way too long to figure out part 2. Essentially just involves me testing to see if any even parts have an aba before reversing it and checking if any odd parts have a corresponding bab. Didn't know that I had to check all odd parts so got working code with the wrong answer multiple times.

--- 2024-11-18-advent2016/AdventOfCode7.java
+++ 2024-11-18-advent2016/AdventOfCode7.java
@@ -6,6 +6,7 @@ public class AdventOfCode7{
 
   public static void main(String[] args) {
     System.out.println(validIPs("Advent7.txt"));
+    System.out.println(supportSSL("Advent7.txt"));
   }
 
   public static int validIPs(String filename){
@@ -37,7 +38,7 @@ public class AdventOfCode7{
       boolean isHypernet = (i % 2 != 0);
       for (int x = 0; x < parts[i].length() - 3; x++) {
         String segment = parts[i].substring(x, x + 4);
-        if (isABBA(segment)) {
+        if (isItABBA(segment)) {
           if (isHypernet) {
             return false; 
           } else {
@@ -49,9 +50,54 @@ public class AdventOfCode7{
     return isABBA;
   }
 
-  public static boolean isABBA(String segment) {
+  public static boolean isItABBA(String segment) {
     return segment.charAt(0) == segment.charAt(3) &&
            segment.charAt(1) == segment.charAt(2) &&
+           segment.charAt(0) != segment.charAt(1);
+  }
+
+  public static int supportSSL(String filename){
+    int sum = 0;
+    try {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+          String line = scanner.nextLine();
+          line = line.replace(']',',');
+          line = line.replace('[',',');
+          String[] parts = line.split(",");
+          if (insideOut(parts)){
+            sum++;
+          }
+        }
+        scanner.close();
+        return sum;
+      }
+    catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      return 0;
+    }
+  }
+
+  public static boolean insideOut(String[] parts){
+    boolean isABA = false;
+    for (int i = 0; i < parts.length; i += 2) {
+      for (int x = 0; x < parts[i].length() - 2; x++) {
+        String segment = parts[i].substring(x, x + 3);
+        if (isItABA(segment)) {
+          String segmentBackwards = segment.substring(1,2) + segment.substring(0,1) + segment.substring(1,2);
+          for (int j = 1; j < parts.length; j += 2) {  
+            if (parts[j].contains(segmentBackwards)) {
+              return true;  
+            }
+          }
+        }
+      }
+    }
+    return isABA;
+  }
+  public static boolean isItABA(String segment) {
+    return segment.charAt(0) == segment.charAt(2) &&
            segment.charAt(0) != segment.charAt(1);
   }
 }
