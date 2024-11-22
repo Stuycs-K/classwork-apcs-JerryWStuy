77f22b38731402b3be027495d91d7b34e8a6ad57
Jerry Wang <jerryw28@nycstudents.net>
2024-11-21 19:48:59 -0500

    Currently has errors over and over again on recusion. Second method should be relatively similar to first but iterates within the marked string.

--- 2024-11-18-advent2016/AdventOfCode9.java
+++ 2024-11-18-advent2016/AdventOfCode9.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class AdventOfCode9{
   public static void main(String[] args) {
     System.out.println(decompressedLength("Advent9.txt"));
+    System.out.println(decompressedLengthTwo("Advent9.txt"));
   }
 
   public static int decompressedLength(String filename){
@@ -24,6 +25,22 @@ public class AdventOfCode9{
     }
   }
 
+  public static long decompressedLengthTwo(String filename){
+    String entireString = "";
+    try {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+          entireString += scanner.nextLine();
+        }
+        scanner.close();
+        return actualDecompressedLengthTwo(entireString);
+      }
+    catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      return 0;
+    }
+  }
 
   public static int actualDecompressedLength(String entireString){
     int length = 0;
@@ -46,6 +63,31 @@ public class AdventOfCode9{
     return length;
   }
 
+  public static long actualDecompressedLengthTwo(String entireString) {
+    long length = 0;
+    int i = 0;
+
+    while (i < entireString.length()) {
+        char current = entireString.charAt(i);
+        if (current == '(') {
+            int closingParentheses = entireString.indexOf(')', i);
+            String marker = entireString.substring(i + 1, closingParentheses);
+            String[] parts = marker.split("x");
+            int numChars = Integer.parseInt(parts[0]);
+            int repeatCount = Integer.parseInt(parts[1]);
+
+            int newMarkerStart = closingParentheses + 1;
+            int newMarkerEnd = newMarkerStart + numChars;
+            long decompressedLength = actualDecompressedLengthTwo(entireString);
+            length += decompressedLength * repeatCount;
+            i = newMarkerEnd;
+        } else {
+            length++;
+            i++;
+        }
+    }
+    return length;
+}
 
   
 }
