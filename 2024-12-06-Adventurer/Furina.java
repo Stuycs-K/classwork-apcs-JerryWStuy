aed3f7dd771ddc22353337198895372d1dd73f11
Jerry Wang <jerryw28@nycstudents.net>
2024-12-08 13:49:00 -0500

    Wrote my entire file. Overrode the name method to contain my nickname variable. Not currently tested but should work.

---
+++ 2024-12-06-Adventurer/Furina.java
@@ -0,0 +1,64 @@
+public class Furina extends Adventurer {
+    private String nickname = "";
+    private int fanfareStacks = 0;
+    private int damage = 0;
+
+    public Furina(String name){
+        super("Furina");
+        nickname = name;
+    }
+    public Furina(String name, int hp){
+        super("Furina",hp);
+        nickname = name;
+    }
+
+    public int restoreSpecial(int n){
+        if( n > getSpecialMax() - getSpecial()){
+                n = getSpecialMax() - getSpecial();
+        }
+        setSpecial(getSpecial()+n);
+        damage = fanfareStacks * 2;
+        return n;
+    }
+
+    public String getSpecialName(){
+        return("Fanfare");
+    }
+    public int getSpecial(){
+        return fanfareStacks;
+    }
+    public void setSpecial(int n){
+        if (n < getSpecialMax()){
+            fanfareStacks = n;
+        }
+        damage = fanfareStacks * 2;
+    }
+    public int getSpecialMax(){
+        return 400;
+    }
+    @Override
+    public String getName(){
+        return("My name is Furina, also known as Focalors. My nickname is: " + nickname);
+    }
+
+    public String attack(Adventurer other){
+        other.setHP(other.getHP()- damage);
+        return (other.getName() + " just took " + damage + " damage from a normal attack.");
+    }
+    
+    public String support(Adventurer other){
+        this.setHP(this.getHP()-5);
+        other.setHP(other.getHP()+fanfareStacks*10);
+        return("Healed you for " + fanfareStacks*10 + "hp.");
+    }
+
+    public String support(){
+        this.setHP(this.getHP()+fanfareStacks*10);
+        return("Healed myself for " + fanfareStacks*10 + "hp.");
+    }
+
+    public String specialAttack(Adventurer other){
+        other.setHP(other.getHP()- damage*5);
+        return (other.getName() + " just took " + damage*5 + " damage from a bubble attack.");
+    }
+}
