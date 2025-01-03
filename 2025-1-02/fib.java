d6c6e53e176de22f93d4352e3e0ccd1653a34a6e
Jerry Wang <jerryw28@nycstudents.net>
2025-01-03 11:35:33 -0500

    Finished CW. Making up 1/2/2025 CW because I was absent Thursday.

---
+++ 2025-1-02/fib.java
@@ -0,0 +1,13 @@
+public class fib{
+  public static void main(String[] args) {
+    System.out.println(fib(45));
+  }
+  public static int fib(int n){
+    if (n == 1 || n == 2){
+      return 1;
+    }
+    else{
+      return fib(n-1) + fib(n-2);
+    }
+  }
+}
